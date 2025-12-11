ffa335bdbcd786df7504bdc638414f9edd175c3a
kaan177 <66557991+kaan177@users.noreply.github.com>
2025-12-11 15:45:59 +0100

    opdracht 2

--- P2/starting-framework/src/lib/Model.hs
+++ P2/starting-framework/src/lib/Model.hs
@@ -1,10 +1,18 @@
 module Model where
 
+
 -- Exercise 1
 data Token = TArrow | TPeriod | TComma | TGo | TTake | TMark 
  | TNothing | TTurn | TCase | TOf | TLeft | TRight | TFront
  | TSemicolon | TEmpty | TLambda | TDebris | TAsteroid
  | TBoundary | TUnderscore | TIdent String deriving Show
 
+
 -- Exercise 2
 data Program = Program deriving Show
+
+data Rule = Rule (String -> Cmds)
+data Cmds = EmptyCmds | Cmds Cmd [Cmd]
+data Cmd = Go | Take | Mark | NothingCmd | Turn Dir | Case (Dir -> Pat -> Cmds)
+data Dir = Left | Right | Front
+data Pat = EmptyPat | LambdaPat | DebrisPat | AsteroidPat | BoundaryPat | CatchAllPat
