b611795579ccfd817bbcacf1384e243c085b6ee0
kaan177 <66557991+kaan177@users.noreply.github.com>
2025-12-20 14:58:56 +0100

    interactive driver

--- P2/starting-framework/src/app/Main.hs
+++ P2/starting-framework/src/app/Main.hs
@@ -8,10 +8,22 @@ import Parser
 
 -- Exercise 11
 interactive :: Environment -> ArrowState -> IO ()
-interactive = undefined
+interactive e a = case (step e a) of
+    (Done s _ _) -> print ("final state: " ++ "\r\n" ++ "\r\n" ++ printSpace s ++ "\r\n")
+    (Fail errormessage)     -> print ("error: " ++ errormessage)
+    (Ok (ArrowState s p h st)) -> print (printSpace s ++ "\r\n" ++ "\r\n" ++ "press enter to continue... " ++ "\r\n") >> nextstep e (ArrowState s p h st) 
+
+
+nextstep :: Environment -> ArrowState -> IO ()
+nextstep e a = do
+  line <- getLine
+  if null line
+    then print("\r\n") >> interactive e a
+    else nextstep e a
+
 
 batch :: Environment -> ArrowState -> (Space, Pos, Heading)
-batch = undefined
+batch e a = undefined
 
 -- This function is just here to play around with and test your lexer/parser.
 -- When implementing exercise 11, delete this comment and this function,
